f97756d883a2114c7f2f268312d8f42fd791d22a
oscivan84 <oscar.diazpt@gmail.com>
2025-05-03 23:25:59 -0500

    Primer commit del proyecto ConectaN8N

---
+++ packages/provider-baileys/src/baileyWrapper.ts
@@ -0,0 +1,40 @@
+import {
+    useMultiFileAuthState,
+    DisconnectReason,
+    proto,
+    makeInMemoryStore,
+    makeCacheableSignalKeyStore,
+    getAggregateVotesInPollMessage,
+    WASocket,
+    BaileysEventMap,
+    AnyMediaMessageContent,
+    AnyMessageContent,
+    PollMessageOptions,
+    downloadMediaMessage,
+    WAMessage,
+    MessageUpsertType,
+    isJidGroup,
+    isJidBroadcast,
+} from '@whiskeysockets/baileys'
+
+const makeWASocketOther = require('@whiskeysockets/baileys').default
+
+export {
+    makeWASocketOther,
+    useMultiFileAuthState,
+    DisconnectReason,
+    proto,
+    makeInMemoryStore,
+    makeCacheableSignalKeyStore,
+    getAggregateVotesInPollMessage,
+    WASocket,
+    BaileysEventMap,
+    AnyMediaMessageContent,
+    AnyMessageContent,
+    PollMessageOptions,
+    downloadMediaMessage,
+    WAMessage,
+    MessageUpsertType,
+    isJidGroup,
+    isJidBroadcast,
+}
---
+++ packages/provider-baileys/src/type.ts
@@ -0,0 +1,15 @@
+import type { GlobalVendorArgs } from '@builderbot/bot/dist/types'
+
+export interface BaileyGlobalVendorArgs extends GlobalVendorArgs {
+    gifPlayback: boolean
+    usePairingCode: boolean
+    phoneNumber: string | null
+    browser: string[]
+    experimentalSyncMessage?: string
+    useBaileysStore: boolean
+    timeRelease?: number
+    experimentalStore?: boolean
+    groupsIgnore: boolean
+    readStatus: boolean
+    host?: any
+}
